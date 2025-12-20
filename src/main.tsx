0f5249128545e9eb99c20eb77c5cc1b26df4cf6b
OjuoyeMoshoodOlawale <ojuoye1999@gmail.com>
2025-12-20 23:42:03 +0100

    Update app

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom"; // <-- important
+import { HashRouter } from "react-router-dom"; // changed
 import App from "./App.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
